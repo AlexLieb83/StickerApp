c9cb4929bdac20cecb82266f5ee11af49e207c1f
AlexLieb83 <alexliebhaber1992@gmail.com>
2025-11-12 20:21:51 -0600

    Initial Commit

---
+++ StickerApp/StickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerAppApp.swift
+//  StickerApp
+//
+//  Created by Alex Liebhaber on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
